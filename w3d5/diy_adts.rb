acaa06e551a098a1eeff0f948ca2f76b26d74755
jkhbuild <jkh.build@gmail.com>
2022-09-29 21:58:06 -0400

    w3d5 stack class

--- w3d5/diy_adts.rb
+++ w3d5/diy_adts.rb
@@ -3,4 +3,18 @@ class Stack
         @stack = []
     end
 
+    def push(el)
+        @stack.push(el)
+    end
+
+    def pop
+        @stack.pop
+        @stack
+    end
+
+    def peek
+        @stack.last
+    end
+end
+
     
