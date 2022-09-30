edfb1ff6684a6096943db051feab3fe2a13c9ca5
jkhbuild <jkh.build@gmail.com>
2022-09-29 22:32:36 -0400

    w3d5 map class

--- w3d5/diy_adts.rb
+++ w3d5/diy_adts.rb
@@ -1,6 +1,6 @@
 class Stack
-    def initialize
-        @stack = []
+    def initialize(stack = [])
+        @stack = stack
     end
 
     def push(el)
@@ -18,11 +18,11 @@ class Stack
 end
 
 class Queue
-    def initialize
-        @queue = []
+    def initialize(queue = [])
+        @queue = queue
     end
 
-    def enqeue(el)
+    def enqueue(el)
         @queue.unshift(el)
     end
 
@@ -35,11 +35,46 @@ class Queue
         @queue.last
     end
 end
+current_line = ["john", "ginnel", "justin", "peter", "eric"]
+shopping_line = Queue.new(current_line)
+p shopping_line
+shopping_line.enqueue("kevin")
+p shopping_line
+p shopping_line.peek
+p shopping_line.dequeue
+p shopping_line.dequeue
 
 class Map
+    def initialize(map = [])
+        @map = map
+    end
 
+    def set(key, value)
+        pair_index = 0
+        @map.each_with_index do |pair, i| 
+            if pair[0] == key
+                pair_index += i
+            end
+        end
+        
+        if pair_index != 0
+            @map[i].last = value
+        else
+            @map << [key, value]
+        end
+    end
 
+    def get(key)
+        @map.each { |pair| return value if pair[0] == key }
+    end
 
+    def delete(key)
+        @map.select { |pair| pair.first != key }
+    end
+
+    def show
+        @map
+    end
 end
 
     
