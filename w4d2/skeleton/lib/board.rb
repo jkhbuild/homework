557fbf8029beb4d702948e60ca4067978dd99ebb
jkhbuild <jkh.build@gmail.com>
2022-10-03 22:06:15 -0400

    valid_move? created

--- w4d2/skeleton/lib/board.rb
+++ w4d2/skeleton/lib/board.rb
@@ -21,6 +21,9 @@ class Board
   end
 
   def valid_move?(start_pos)
+   raise "Invalid starting cup" if start_pos < 0 || start_pos > 12
+   raise "Starting cup is empty" if @cups[start_pos].empty?
+    
   end
 
   def make_move(start_pos, current_player_name)
