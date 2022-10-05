9f694acf53b92457aeaa8b23386f80e92a04b550
jkhbuild <jkh.build@gmail.com>
2022-10-04 21:55:34 -0400

    simon skeleton setup

---
+++ w4d3/skeleton/lib/simon.rb
@@ -0,0 +1,56 @@
+class Simon
+  COLORS = %w(red blue green yellow)
+
+  attr_accessor :sequence_length, :game_over, :seq
+
+  def initialize
+    @seq = []
+    @game_over = false
+    @sequence_length = 1
+  end
+
+  def play
+    until self.game_over
+      self.take_turn
+    end
+
+  end
+
+  def take_turn
+    if self.game_over
+      self.show_sequence
+      self.require_sequence
+    else
+      self.round_success_message
+      self.sequence_length += 1
+    end
+
+  end
+
+  def show_sequence
+
+  end
+
+  def require_sequence
+
+  end
+
+  def add_random_color
+    colors_hash = { 1=>"red" , 2=>"blue", 3=>"yellow", 4=>"green" }
+    random = rand(1..4)
+
+    seq << colors_hash[random]
+  end
+
+  def round_success_message
+
+  end
+
+  def game_over_message
+
+  end
+
+  def reset_game
+
+  end
+end
