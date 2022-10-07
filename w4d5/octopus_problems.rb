58be72eb9b896d28b3d612a14e7aa7633f0cf599
jkhbuild <jkh.build@gmail.com>
2022-10-06 22:33:00 -0400

    fast_dance

--- w4d5/octopus_problems.rb
+++ w4d5/octopus_problems.rb
@@ -47,6 +47,15 @@ p slow_dance("up", tiles_array)
 
 p slow_dance("right-down", tiles_array)
 
+new_tiles_data_structure = { "up"=>0, "right-up"=>1, "right"=>2, "right-down"=>3, "down"=>4, "left-down"=>5, "left"=>6,  "left-up"=>7 }
 
+def fast_dance (direction, new_tiles_data_structure)
+    new_tiles_data_structure[direction]
+end
+p fast_dance("up", new_tiles_data_structure)
+
+
+p fast_dance("right-down", new_tiles_data_structure)
+    
     
 
