1848383b5606b5e7b1c457efc1a9cc4e0ac403c2
jkhbuild <jkh.build@gmail.com>
2022-10-06 22:04:02 -0400

    dominant octopus

--- w4d5/octopus_problems.rb
+++ w4d5/octopus_problems.rb
@@ -1,3 +1,4 @@
+require "byebug"
 def sluggish_octopus(arr_fish)
     longest = arr_fish.first
     arr_fish.each do |fish1|
@@ -12,4 +13,14 @@ end
 arr_fish = ['fish', 'fiiish', 'fiiiiish', 'fiiiish', 'fffish', 'ffiiiiisshh', 'fsh', 
  'fiiiissshhhhhh']
  p sluggish_octopus(arr_fish)
-# def dominant octopus
+
+def dominant_octopus(arr_fish)
+    return arr_fish.first if arr_fish.length < 2
+    pivot = arr_fish.first
+    left = arr_fish[1..-1].select { |fish| fish.length < pivot.length }
+    right = arr_fish[1..-1].select { |fish| fish.length > pivot.length}
+    debugger
+    (dominant_octopus(left) + [pivot] + dominant_octopus(right)).last
+end
+ p dominant_octopus(arr_fish)
+
