f1521e9815059d4e43c812f1ef7e16051b02c46b
jkhbuild <jkh.build@gmail.com>
2022-10-23 21:28:13 -0400

    user validations created

--- w7d1/homework/app/models/user.rb
+++ w7d1/homework/app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+    validates :username, presence: true, uniqueness: true
+    validates :password_digest, presence: { message: 'Password cant be blank'}
+    validates :password, length: { minimum: 6, allow_nil: true }
+    before_validation :ensure_session_token
 end
